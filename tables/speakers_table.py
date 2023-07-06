143d4f45dfb7f42b0c9e4bcab056aa0b2af4e6c5
rupin27 <rupinmehra@umass.edu>
2023-07-06 03:52:11 -0400

    Add file: speakers_table.py - Initializing Speaker Class and its attributes.

---
+++ tables/speakers_table.py
@@ -0,0 +1,16 @@
+class Speaker:
+    def __init__(self, name, date, time_start, time_end, session_id):
+        # attributes of a speaker
+        self.name = "speakers"
+        self.date = "date"
+        self.time_start = "time_start"
+        self.time_end = "time_end"
+        self.session_id = "session_title"
+        # dictionary to store the speaker data
+        self.data = {
+            self.name: name,
+            self.date: date,
+            self.time_start: time_start,
+            self.time_end: time_end,
+            self.session_id: session_id
+        }
